695cb9005b3dbe9289d56341174507757c782a2b
theSasster <107024368+SarthakRana21@users.noreply.github.com>
2024-11-25 14:17:29 +0000

    added product list

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,8 @@ const nextConfig = {
             {
                 protocol: "https",
                 hostname: "images.pexels.com"
-            }
+                  
+            },
         ]
     }
 };
---
+++ src/app/components/ProductList.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import Image from "next/image"
+
+const ProductList = () => {
+    return (
+        <div className="flex gap-x-8 gap-y-16 justify-between flex-wrap w-full h-80">
+            <Link href={'/test'} className="relative">
+                <Image src="https://images.pexels.com/photos/29493395/pexels-photo-29493395/free-photo-of-stylish-winter-fashion-in-snowy-landscape.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="test" fill sizes="25vw"/>
+            </Link>
+        </div>
+    )
+}
+export default ProductList;
