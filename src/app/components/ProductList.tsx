042fff31588a56b3da55dff60d04cbe11c875671
theSasster <107024368+SarthakRana21@users.noreply.github.com>
2024-11-25 14:20:15 +0000

    updated productlist

--- src/app/components/ProductList.tsx
+++ src/app/components/ProductList.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 const ProductList = () => {
     return (
         <div className="flex gap-x-8 gap-y-16 justify-between flex-wrap w-full h-80">
-            <Link href={'/test'} className="relative">
+            <Link href={'/test'} className="relative w-full h-80">
                 <Image src="https://images.pexels.com/photos/29493395/pexels-photo-29493395/free-photo-of-stylish-winter-fashion-in-snowy-landscape.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="test" fill sizes="25vw"/>
             </Link>
         </div>
